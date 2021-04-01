d4570a7dc0318ae20ec440b6ad8ec74b4f452114
032605 <032605@naver.com>
2021-04-01 16:59:17 +0900

    could be for 2 more

--- test-opencv.py
+++ test-opencv.py
@@ -7,7 +7,7 @@ openApiURL = "http://aiopen.etri.re.kr:8000/FaceDeID"
 accessKey = "2ce78d65-5d6b-4f40-ae0a-9c0e1b0112cb"
 type = "1";     # 얼굴 비식별화 기능 "1"로 설정
 
-image_path = './src/5.jpg'
+image_path = "./src/6.jpg"
 
 file = open(image_path, "rb")
 imageContents = base64.b64encode(file.read()).decode("utf8")
@@ -30,6 +30,7 @@ response = http.request(
 )
 
 content = json.loads(response.data)
+
 # 이미지 읽기
 img = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
 
@@ -39,19 +40,31 @@ for i in range(0,len(content["return_object"]["faces"])):
     img_width = int(content["return_object"]["faces"][i]["width"])
     img_height = int(content["return_object"]["faces"][i]["height"])
     text = str(img_x) + "," + str(img_y)
+    # 검출된 객체 사각형 그리기 
+    cv2.rectangle(img, (img_x,img_y),(img_x+img_width,img_y+img_height),(255,255,255),4)
+    # 사각형 위에 텍스트 붙이기
+    cv2.putText(img, text, (img_x, img_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+    # 크롭 이미지 저장
+    image_crop = img[img_y : img_y + img_height, img_x : img_x + img_width]
+    cv2.imwrite('./crop.jpg', image_crop)
+    
+    
+    #cv2.putText(img, text,(img_x,img_y -10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,255,255))
+	# 사각형 위에 텍스트 붙이기
+ 	#cv2.putText(img, text,(img_x,img_y -10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+	# 크롭 이미지 저장
+	#cropped_img = img[img_y: img_y + img_height, img_x : img_x + img_width ]
+	#cv2.imwrite('./cropped_img.jpg',cropped_img)
+
 
 # 구하는 방법
 #cv2.rectangle(src, (x, y), (x + w, y + h), (255, 0, 0), 2)
-cv2.rectangle(img, (img_x,img_y),(img_x+img_width,img_y+img_height),(255,0,0),4)
-cv2.putText(img, text,(img_x,img_y -10),
-cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
 
-cropped_img = img[img_y: img_y + img_height, img_x : img_x + img_width ]
-cv2.imwrite('./cropped_img.jpg',cropped_img)
 
 cv2.imshow("rectangle", img)
-
 cv2.waitKey(0)
+
+# json 객체로 반환
 #print("[responseCode] " + str(response.status))
 #print("[responBody]")
-print(response.data)
+#print(response.data)
