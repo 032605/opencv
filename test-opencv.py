5a5875b9e352fa3029042f5eb0729fc8912dcbab
032605 <032605@naver.com>
2021-04-01 14:36:46 +0900

    commit

--- test-opencv.py
+++ test-opencv.py
@@ -5,6 +5,9 @@ image_path = './src/1.jpg'
 # 이미지 읽기
 image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
 
-cv2.rectangle(image, (10,10),(20,20),(255,255,255),2)
-#cv2.imshow("title",image)
-#cv2.waitKey(0)
+img2 = cv2.rectangle(image, (10,10),(20,20),(255,255,255),2)
+
+cv2.imshow("orignal",image)
+cv2.imshow("rectangle", img2)
+
+cv2.waitKey(0)
